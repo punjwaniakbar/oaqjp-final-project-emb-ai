338c27a0aec97a1c4f2254f9e9b698b882100352
Akbar Punjwani <akbarpunjwani@yahoo.com>
2025-05-01 11:42:30 -0400

    Incorporated Error handling and Run static code analysis

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+"""Flask Server for Web Deployment"""
 from flask import Flask, render_template, request
 from EmotionDetection import emotion_detector
 
@@ -5,15 +6,17 @@ app = Flask("Emotion Detection")
 
 @app.route('/')
 def render_homepage():
+    """ Render homepage """
     return render_template("index.html")
 
 @app.route('/emotionDetector', methods=["GET"])
 def emotion_detection():
+    """ Analyze text and return emotion detection result """
     text_to_analyze = request.args["textToAnalyze"]
     analysis_result = emotion_detector(text_to_analyze)
 
     if analysis_result["dominant_emotion"] is None:
-        response = "Could not detect emotions! Please try again!"
+        response = "Invalid text! Please try again!"
     else:
         response = "For the given statement, the system response is "
 
@@ -25,9 +28,10 @@ def emotion_detection():
         if last_comma_index != -1:
             response = response[:last_comma_index] + '.' + response[last_comma_index + 1:]
 
-        response += f" The dominant emotion is {analysis_result['dominant_emotion']}."
+        response += f" The dominant emotion is <b>{analysis_result['dominant_emotion']}</b>."
 
     return response
 
 if __name__ == "__main__":
     app.run(debug = True)
+    
