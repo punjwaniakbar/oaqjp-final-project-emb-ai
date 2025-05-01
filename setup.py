510fdd061912cf44cf2c36e50abc3ed9fce82260
punjwaniakbar <akbarpunjwani@yahoo.com>
2025-05-01 03:53:47 -0400

    Package Initiation done

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='EmotionDetection',
+    version='0.1',
+    packages=find_packages(),
+    description='Predict emotions in the text message',
+    author='Akbar Punjwani',
+    author_email='akbarpunjwani@yahoo.com',
+    url='https://github.com/punjwaniakbar/oaqjp-final-project-emb-ai',  
+    install_requires=[],  # List dependencies if any
+)
